2a4d3a9147c2ddd50a60d30b86b27706eb375332
Mike Alberts <mikealberts@gmail.com>
2023-08-30 19:07:35 -0400

    Small page edits

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Free Domain - Free Hosting - Free Everything</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to the test center!" />
+        <Header title="Free next.js Site" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Just a little demo.
         </p>
       </main>
 
